1be7d1c0c19dd071ad999e139809decac9e545b9
Andreas Barth <aba@not.so.argh.org>
2010-06-17 16:42:03 +0000

    adjust git clone URL

--- library.php
+++ library.php
@@ -439,7 +439,7 @@ Page generated on $date UTC<br />
 Pages written by <a href=\"http://wiki.debian.org/MehdiDogguy\">Mehdi Dogguy</a>
 (based on old status pages written by Jeroen van Wolffelaar)<br />
 Pages maintained by the wanna-build team &lt;debian-wb-team@lists.debian.org&gt;<br />
-Download code with git: <tt>git clone http://buildd.debian.org/~mehdi/pgstatus/.git</tt>
+Download code with git: <tt>git clone http://buildd.debian.org/git/pgstatus.git</tt>
 </small>
 </body>
 </html>";
