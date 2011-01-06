28847352688905882f4430af05bf4985fc648030
Mehdi Dogguy <mehdi@debian.org>
2011-01-06 21:47:27 +0000

    Check suite in package.php

--- package.php
+++ package.php
@@ -2,19 +2,19 @@
 
 require_once("library.php");
 
-$suite = $_GET["suite"];
 $package = preg_replace ('/[^-a-z0-9\+\., ]/', '', $_GET["p"]);
-$archs = $_GET["a"];
-$compact = !empty($_GET["compact"]);
 $packages = preg_split('/[ ,]+/', $package);
 
 html_header(count($packages) > 1);
+
+$suite = check_suite($_GET["suite"]);
+$archs = $_GET["a"];
+$compact = !empty($_GET["compact"]);
 
 page_header($packages);
 
 echo "<div id=\"body\">\n";
 
-alert_if_neq("suite", $suite, $_GET["suite"]);
 alert_if_neq("architecure", $archs, $_GET["a"]);
 
 if (!empty($package)) pkg_links($packages, $suite);
