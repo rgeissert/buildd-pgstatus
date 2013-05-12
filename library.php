rgeissert/buildd-pgstatus

commit 6c91e2b391e15512d0103b6c0cf3af42aadcfb0a

Add ability to filter on suite in logs.php

library.php
@@ -157,21 +157,22 @@ function string_query($package, $suite, $fields="", $extra="") {
   return sprintf($format, $fields, $suite, $package, $extra);
 }
 
-function log_query_arch($pkg, $arch, $ver="") {
+function log_query_arch($pkg, $arch, $ver="", $suite="") {
   return sprintf("SELECT '%s'::character varying AS arch, CAST (version AS debversion) AS debversion, *
                   FROM \"%s_public\".pkg_history
                   WHERE package = '%s'
-                  %s",
-		 $arch, $arch, $pkg, $ver);
+                  %s %s",
+		 $arch, $arch, $pkg, $ver, $suite);
 }
 
-function log_query($pkg, $archs, $ver) {
+function log_query($pkg, $archs, $ver, $suite) {
   global $ARCHS;
   if (empty($archs)) $archs = $ARCHS;
   if (!empty($ver)) $ver = sprintf(" AND version = '%s'", $ver);
-  $query = log_query_arch($pkg, array_shift($archs), $ver);
+  if (!empty($suite)) $suite = sprintf(" AND distribution = '%s'", $suite);
+  $query = log_query_arch($pkg, array_shift($archs), $ver, $suite);
   foreach($archs as $arch) {
-    $query .= sprintf(" UNION %s", log_query_arch($pkg, $arch, $ver));
+    $query .= sprintf(" UNION %s", log_query_arch($pkg, $arch, $ver, $suite));
   }
   return sprintf("%s ORDER BY debversion DESC, arch ASC, timestamp DESC", $query);
 }
@@ -305,8 +306,18 @@ function sanitize_params() {
     case "suite":
       $key = "suite";
       if (!array_key_exists($key, $_GET) || empty($_GET[$key])) $key = "dist";
-      if (array_key_exists($key, $_GET)) array_push($result, check_suite($_GET[$key]));
-      if (!array_key_exists($key, $_GET)) array_push($result, check_suite(""));
+      if (array_key_exists($key, $_GET))
+        array_push($result, check_suite($_GET[$key]));
+      else
+        array_push($result, check_suite(""));
+      break;
+    case "dist":
+      $key = "dist";
+      if (!array_key_exists($key, $_GET) || empty($_GET[$key])) $key = "suite";
+      if (array_key_exists($key, $_GET))
+        array_push($result, check_suite($_GET[$key]));
+      else
+        array_push($result, check_suite("", ""));
       break;
     case "compact":
       array_push($result, array_key_exists("compact", $_GET) && !empty($_GET["compact"]));
@@ -501,13 +512,15 @@ function logsize($size) {
   return round($size, 2).$sep.$unit;
 }
 
-function logs_link($pkg, $arch, $ver="", $text="old") {
+function logs_link($pkg, $arch, $ver="", $suite="", $text="old") {
   if (!empty($ver)) $ver = sprintf("&amp;ver=%s", urlencode($ver));
   if (!empty($arch)) $arch = sprintf("&amp;arch=%s", urlencode($arch));
-  return sprintf("<a href=\"logs.php?pkg=%s%s%s\">%s</a>",
+  if (!empty($suite)) $suite = sprintf("&amp;suite=%s", urlencode($suite));
+  return sprintf("<a href=\"logs.php?pkg=%s%s%s%s\">%s</a>",
 		 urlencode($pkg),
 		 $ver,
 		 $arch,
+		 $suite,
 		 $text
 		 );
 }
@@ -516,7 +529,7 @@ function loglink($package, $version, $arch, $timestamp, $count, $failed) {
   global $pendingstate;
   $log = "";
   $old = logs_link($package, $arch);
-  $all = logs_link($package, $arch, $version, sprintf("all (%d)", htmlentities($count)));
+  $all = logs_link($package, $arch, $version, "", sprintf("all (%d)", htmlentities($count)));
   if (empty($timestamp) || $count == 0)
     $log = "no log";
   else if ($failed) {
@@ -1138,7 +1151,7 @@ function buildd_status($packages, $suite, $archis=array()) {
       if ($info["state"] == "Installed" && $log == "no log") $info["timestamp"] = "";
       pkg_history($package, $version, $arch, $suite);
 
-      if ($log == "no log") $log = sprintf("%s | %s", logs_link($package, $arch), $log);
+      if ($log == "no log") $log = sprintf("%s | %s", logs_link($package, $arch), "", $log);
       $print($info, $version, $log, $arch, $suite, $problemid);
 
       // There is no need to repeat the same message for all selected architectures.

fetch.php
@@ -53,8 +53,8 @@ if (!file_exists($path)) {
 
     $links = sprintf("%s → %s → %s → %s\n",
 		     sprintf("<a href=\"package.php?p=%s\">%s</a>", urlencode($pkg), $pkg),
-		     logs_link($pkg, "", $ver, $ver),
-		     logs_link($pkg, $arch, $ver, $arch),
+		     logs_link($pkg, "", $ver, "", $ver),
+		     logs_link($pkg, $arch, $ver, "", $arch),
 		     fdate($stamp)
 		     );
     ifecho($links);

logs.php
@@ -23,17 +23,17 @@ require_once("library.php");
 db_connect();
 
 list($pkg, $ver, $arch, $suite, $stamp) =
-  sanitize_params("pkg", "ver", "arch", "suite", "stamp");
+  sanitize_params("pkg", "ver", "arch", "dist", "stamp");
 if (empty($arch))
   $arch = array();
 else
   $arch = array($arch);
 
 function next_version($version, $lastver, $found) {
-  global $pkg, $ver, $arch;
+  global $pkg, $ver, $arch, $suite;
   if (!$found || $version != $lastver)
     if ($version != $ver)
-      return logs_link($pkg, safe_get($arch, 0), $version, $version);
+      return logs_link($pkg, safe_get($arch, 0), $version, $suite, $version);
     else
       return $version;
   else
@@ -41,12 +41,12 @@ function next_version($version, $lastver, $found) {
 }
 
 function next_arch($archi, $lastarch, $found) {
-  global $pkg, $ver, $arch;
+  global $pkg, $ver, $arch, $suite;
   if ($found && $lastarch == $archi) return "";
   if (count($arch) > 0)
     return $archi;
   else
-    return logs_link($pkg, $archi, $ver, $archi);
+    return logs_link($pkg, $archi, $ver, $suite, $archi);
 }
 
 html_header(sprintf("Build logs for %s%s%s",
@@ -62,7 +62,7 @@ echo "<div style=\"float:right\">";
 select_logs($pkg);
 echo "</div>";
 
-pkg_links(array($pkg), "sid");
+pkg_links(array($pkg), check_suite($suite));
 
 if (empty($pkg)) {
   echo "<h3>Please enter a package name there --></h3>\n";
@@ -70,15 +70,19 @@ if (empty($pkg)) {
   printf("<h3>Build logs for <a href=\"package.php?p=%s\">%s</a>", urlencode($pkg), $pkg);
   if (!empty($ver)) {
     echo "_$ver";
-    printf(" <small>[%s]</small>", logs_link($pkg, safe_get($arch, 0), "", "X"));
+    printf(" <small>[%s]</small>", logs_link($pkg, safe_get($arch, 0), "", $suite, "X"));
   }
   if (!empty($arch)) {
     printf(" on <a href=\"architecture.php?a=%s\">%s</a>", $arch[0], $arch[0]);
-    printf(" <small>[%s]</small>", logs_link($pkg, "", $ver, "X"));
+    printf(" <small>[%s]</small>", logs_link($pkg, "", $ver, $suite, "X"));
+  }
+  if (!empty($suite)) {
+    printf(" in %s", $suite);
+    printf(" <small>[%s]</small>", logs_link($pkg, safe_get($arch, 0), $ver, "", "X"));
   }
   echo "</h3>\n";
 
-  $query = log_query($pkg, $arch, $ver);
+  $query = log_query($pkg, $arch, $ver, $suite);
   $query_result = pg_query($dbconn, $query);
   $found = false;
   $lastver = "";
@@ -109,14 +113,16 @@ if (empty($pkg)) {
     $duration = date_diff_details(0, $r["build_time"]);
     $disk_space = logsize($r["disk_space"]);
     $builder = $r["builder"];
+    $suite = $r["distribution"];
     if (empty($builder)) {
       $builder = no_empty_text("");
     } else {
-      $builder = pkg_buildd(buildd_realname($builder, $r["arch"]), "sid", $r["arch"]);
+      $builder = pkg_buildd(buildd_realname($builder, $r["arch"]), check_suite($suite), $r["arch"]);
     }
 
     $version = next_version($r["version"], $lastver, $found);
     $architecture = next_arch($r["arch"], $lastarch, $found);
+    $suite = logs_link($pkg, safe_get($arch, 0), $ver, $suite, $suite);
     printf("<tr>
             <td%s>%s</td>
             <td%s>%s</td>
@@ -127,7 +133,7 @@ if (empty($pkg)) {
             <td>%s</td>
            </tr>\n",
 	   (empty($version) ? " class=\"empty\" " : ""),
-	   $version,
+	   (empty($version) ? "" : "$version ($suite)"),
 	   (empty($architecture) ? " class=\"empty\" " : ""),
 	   $architecture,
 	   $link,

commit efca80f976e928b4ef434d8e8a7565d4c3cbf692

Fix bug introduced in 6c91e2b391e15512d0103b6c0cf3af42aadcfb0a

library.php
@@ -1162,7 +1162,7 @@ function buildd_status($packages, $suite, $archis=array()) {
       if ($info["state"] == "Installed" && $log == "no log") $info["timestamp"] = "";
       pkg_history($package, $version, $arch, $suite);
 
-      if ($log == "no log") $log = sprintf("%s | %s", logs_link($package, $arch), "", $log);
+      if ($log == "no log") $log = sprintf("%s | %s", logs_link($package, $arch, ""), $log);
       $print($info, $version, $log, $arch, $suite, $problemid);
 
       // There is no need to repeat the same message for all selected architectures.